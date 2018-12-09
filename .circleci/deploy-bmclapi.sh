0c00031f8143fc3c887f66b433117231f602e433
yushijinhun <yushijinhun@gmail.com>
2018-12-09 12:34:07 +0800

    Fix circleci

--- .circleci/deploy-bmclapi.sh
+++ .circleci/deploy-bmclapi.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-mkdir /tmp/bmclapi-deploy
+set -e
+
+if [ -d ~/bmclapi ];then
+	pushd ~/bmclapi
+	git fetch origin
+	git add -A
+	git reset --hard origin/bmclapi
+	popd
+else
+	git clone --single-branch -b bmclapi 'git@github.com:yushijinhun/authlib-injector.yushi.moe' ~/bmclapi
+fi
+
 git --work-tree=/tmp/bmclapi-deploy checkout master -- .
 for artifact in $(find 'artifact' -type f -name '*.json'); do
 	jq '.download_url|=sub("^https://authlib-injector\\.yushi\\.moe/";"https://bmclapi2.bangbang93.com/mirrors/authlib-injector/")' < $artifact > /tmp/bmclapi-deploy/$artifact
 done
-git symbolic-ref HEAD refs/heads/bmclapi
+
+pushd ~/bmclapi
+git config --local user.name 'authlib-injector Deploy Bot'
+git config --local user.email 'authlib-injector-deploy-bot@yushi.moe'
 git --work-tree=/tmp/bmclapi-deploy add --all
 git commit -m 'Deploy BMCLAPI mirror'
-git push 'git@github.com:yushijinhun/authlib-injector.yushi.moe' bmclapi:bmclapi
-git symbolic-ref HEAD refs/heads/master
+echo git push 'git@github.com:yushijinhun/authlib-injector.yushi.moe' bmclapi:bmclapi
+popd
